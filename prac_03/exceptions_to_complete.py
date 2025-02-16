7271317d3ee1f5af3047287662047af1e2926f83
Dream <shuhan.li@my.jcu.edu.au>
2025-02-16 23:30:39 +0800

    Finish todo lines

--- prac_03/exceptions_to_complete.py
+++ prac_03/exceptions_to_complete.py
@@ -4,10 +4,11 @@ Fill in the TODOs to complete the task
 """
 
 is_finished = False
+result = ""
 while not is_finished:
     try:
         result = int(input("Enter a valid integer: "))
-        # DO: this line
-    except:  # DO - add the exception you want to catch after except
+        is_finished = True
+    except ValueError:
         print("Please enter a valid integer.")
 print("Valid result is:", result)
