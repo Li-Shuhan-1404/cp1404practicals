c0fb26a9a15c940fe33de08e891262daca80e6dc
Dream <shuhan.li@my.jcu.edu.au>
2025-03-23 22:51:59 +0800

    Complete read, add, save guitar in file

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -9,16 +9,38 @@ FILENAME = "guitars.csv"
 
 
 def mian():
-    """"""
-    data = open_file(FILENAME)
-    data.sort()
-    for a in data:
-        print(a)
-
-
-def open_file(filename):
+    """Add guitar and write in the list"""
     guitars = []
-    with open(filename, "r") as in_file:
+    print("My guitars!")
+    read_file(guitars)
+    get_guitar(guitars)
+
+    guitars.sort()
+
+    if guitars:
+        print("There are my guitars:")
+
+        for i, guitar in enumerate(guitars, 1):
+            print(f"Guitar {i}: {guitar.name:>25} ({guitar.year}), worth ${guitar.cost:10,.2f}")
+        save_guitar(guitars)
+    else:
+        print("No guitars")
+
+
+def get_guitar(guitars):
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar_add = Guitar(name, year, cost)
+        guitars.append(guitar_add)
+        print(f"{guitar_add} added.")
+        name = input("Name: ")
+
+
+def read_file(guitars):
+    """Get guitar from file"""
+    with open(FILENAME, 'r') as in_file:
         reader = csv.reader(in_file)
         for line in reader:
             name = line[0]
@@ -26,11 +48,13 @@ def open_file(filename):
             cost = float(line[2])
             guitar = Guitar(name, year, cost)
             guitars.append(guitar)
-    return guitars
 
 
-
-
+def save_guitar(guitars):
+    """Save guitar to the file"""
+    with open(FILENAME, 'w') as save_file:
+        for guitar in guitars:
+            print(f"{guitar.name},{guitar.year},{guitar.cost}", file=save_file)
 
 
 mian()
