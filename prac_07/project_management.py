19903a7507b11352c07d38af626ae620b33ac5d0
Dream <shuhan.li@my.jcu.edu.au>
2025-03-25 21:18:28 +0800

    Add display project function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -7,28 +7,29 @@ Actual:   34 minutes
 
 from prac_07.project import Project
 
+TITLE = "Name\tStart Date\tPriority\tCost Estimate\tCompletion Percentage"
 MENU = "- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date" \
        "\n- (A)dd new project\n- (U)pdate project\n- (Q)uit"
 
 
 def main():
-    file = "projects.txt"
-    projects = load_file(file)
+    filename = "projects.txt"
+    projects = load_file(filename)
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "L":
-            projects = load_file(file)
+            projects = load_file(filename)
         elif choice == "S":
-            save_project(file, projects)
+            save_project(filename, projects)
         elif choice == "D":
             display_project(projects)
-        elif choice == "F":
-            projects = filter_project(projects)
-        elif choice == "A":
-            add_project(projects)
-        elif choice == "U":
-            projects = update_project(projects)
+        # elif choice == "F":
+        #     projects = filter_project(projects)
+        # elif choice == "A":
+        #     add_project(projects)
+        # elif choice == "U":
+        #     projects = update_project(projects)
         else:
             print("Invalid choice")
         print(MENU)
@@ -36,10 +37,33 @@ def main():
     print("Thank you for using custom-built project management software.")
 
 
-def load_file(file):
+def display_project(projects):
+    """Display not or complete project"""
+    incomplete_project = []
+    complete_project = []
+    projects.sort()
+    for project in projects:
+        if project.complete == 100:
+            complete_project.append(project)
+        else:
+            incomplete_project.append(project)
+    print("Incomplete projects:")
+    display_project_detail(incomplete_project)
+    print("Complete projects:")
+    display_project_detail(complete_project)
+
+
+def display_project_detail(projects):
+    """String for output project"""
+    for project in projects:
+        print(f" {project.name}, start: {project.start_date}, priority {project.priority}, estimate: ${project.cost}, "
+              f"completion: {project.complete}%")
+
+
+def load_file(filename):
     """Load data from file"""
     projects = []
-    in_file = open(file, 'r')
+    in_file = open(filename, 'r')
     in_file.readline()
     for line in in_file:
         part = line.strip().split('\t')
@@ -48,10 +72,10 @@ def load_file(file):
     return projects
 
 
-def save_project(file, projects):
+def save_project(filename, projects):
     """Save project into file"""
-    with open(file, 'w') as out_file:
-        print("Name\tStart Date\tPriority\tCost Estimate\tCompletion Percentage", file=out_file)
+    with open(filename, 'w') as out_file:
+        print(TITLE, file=out_file)
         for line in projects:
             print(line, file=out_file)
 
