17a369ef16b47a880d5487f362a6e79c3bea07ec
Dream <shuhan.li@my.jcu.edu.au>
2025-03-25 20:49:25 +0800

    Add load and save file function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,19 +1,26 @@
+"""
+CP1404/CP5632 Practical
+Word Occurrences
+Estimate: 120 minutes
+Actual:   34 minutes
+"""
+
 from prac_07.project import Project
 
 MENU = "- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date" \
        "\n- (A)dd new project\n- (U)pdate project\n- (Q)uit"
-FILENAME = "projects.txt"
 
 
 def main():
-    projects = read_file(FILENAME)
+    file = "projects.txt"
+    projects = load_file(file)
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "L":
-            projects = read_file(FILENAME)
+            projects = load_file(file)
         elif choice == "S":
-            save_project(projects)
+            save_project(file, projects)
         elif choice == "D":
             display_project(projects)
         elif choice == "F":
@@ -28,6 +35,25 @@ def main():
         choice = input(">>> ").upper()
     print("Thank you for using custom-built project management software.")
 
+
+def load_file(file):
+    """Load data from file"""
+    projects = []
+    in_file = open(file, 'r')
+    in_file.readline()
+    for line in in_file:
+        part = line.strip().split('\t')
+        projects.append(Project(part[0], part[1], int(part[2]), float(part[3]), int(part[4])))
+    in_file.close()
+    return projects
+
+
+def save_project(file, projects):
+    """Save project into file"""
+    with open(file, 'w') as out_file:
+        print("Name\tStart Date\tPriority\tCost Estimate\tCompletion Percentage", file=out_file)
+        for line in projects:
+            print(line, file=out_file)
 
 
 main()
