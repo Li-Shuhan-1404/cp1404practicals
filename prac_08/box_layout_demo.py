594a6b87aeae08e487028511c6a13cfa1cc1ab2d
Dream <shuhan.li@my.jcu.edu.au>
2025-04-06 19:02:19 +0800

    Complete output format and clear input function

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -4,9 +4,19 @@ from kivy.lang import Builder
 
 class BoxLayoutDemo(App):
     def build(self):
+        """Run the program"""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        """Convert input to output format"""
+        print("test")
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def clear_text(self, input_name):
+        """Clear input and output"""
+        self.root.ids.output_label.text = ""
+        input_name.text = ""
 
 BoxLayoutDemo().run()
